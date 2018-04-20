d07d679bc7ed20bbe2519871d0554deb46bc6b98
TheNerdicCoder <johan@efdapps.com>
2018-04-21 01:27:20 +0300

    Edit the Hero

--- src/components/heroes/heroes.tsx
+++ src/components/heroes/heroes.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 import { Hero } from '../../models/hero';
 
 @Component({
@@ -7,17 +7,28 @@ import { Hero } from '../../models/hero';
 })
 export class Heroes {
 
-  private hero:Hero = {
+  @State() private hero:Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  handleChangeName(event) {
+    this.hero = {
+      id: this.hero.id,
+      name: event.target.value
+    };
+  }
 
   render() {
     return (
 <div class='app-heroes'>
   <h2>{ this.hero.name.toUpperCase() } Details</h2>
   <div><span>id: </span>{this.hero.id}</div>
-  <div><span>name: </span>{this.hero.name}</div>
+  <div>
+    <label>name:
+      <input type="text" value={this.hero.name} onInput={(event) => this.handleChangeName(event)} placeholder="name" />
+    </label>
+  </div>
 </div>
     );
   }
