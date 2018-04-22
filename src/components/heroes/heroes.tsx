7b20b6c77f62413b6ca8a5595e9b1b662169c3e5
TheNerdicCoder <johan@efdapps.com>
2018-04-22 21:00:00 +0300

    Display a Heroes List

--- src/components/heroes/heroes.tsx
+++ src/components/heroes/heroes.tsx
@@ -1,5 +1,6 @@
 import { Component, State } from '@stencil/core';
 import { Hero } from '../../models/hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   tag: 'app-heroes',
@@ -7,28 +8,47 @@ import { Hero } from '../../models/hero';
 })
 export class Heroes {
 
-  @State() private hero:Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  @State() private selectedHero: Hero;
+
+  private heroes = HEROES;
 
   handleChangeName(event) {
-    this.hero = {
-      id: this.hero.id,
+    this.selectedHero = {
+      id: this.selectedHero.id,
       name: event.target.value
     };
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   render() {
+    const heroDetails = this.selectedHero ? (
+      <div>
+      <h2>{ this.selectedHero.name.toUpperCase() } Details</h2>
+      <div><span>id: </span>{this.selectedHero.id}</div>
+      <div>
+        <label>name:
+          <input type="text" value={this.selectedHero.name} onInput={(event) => this.handleChangeName(event)} placeholder="name" />
+        </label>
+      </div>
+      </div>
+    ) : (
+      <p>Select a hero!</p>
+    );
+
     return (
 <div class='app-heroes'>
-  <h2>{ this.hero.name.toUpperCase() } Details</h2>
-  <div><span>id: </span>{this.hero.id}</div>
-  <div>
-    <label>name:
-      <input type="text" value={this.hero.name} onInput={(event) => this.handleChangeName(event)} placeholder="name" />
-    </label>
-  </div>
+  <h2>My Heroes</h2>
+  <ul class="heroes">
+    {this.heroes.map((hero) =>
+      <li class={ (this.selectedHero === hero ? 'selected' : '')} onClick={ () => this.onSelect(hero)}>
+        <span class="badge">{hero.id}</span> {hero.name}
+      </li>
+      )}
+  </ul>
+  {heroDetails}
 </div>
     );
   }
